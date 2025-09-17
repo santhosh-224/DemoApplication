88e89a627fb75deb6454c9bd64388b7fcd238a0c
Santhosh <santhoshsanthanaraman224@gmail.com>
2025-09-18 02:25:23 +0530

    Week 4: Data Transfer Objects and Model Mappers

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.UserDTO;
 import com.example.demo.dto.UserRequest;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -17,7 +17,12 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<User> saveUser(@RequestBody UserRequest request) {
+    public ResponseEntity<UserDTO> saveUser(@RequestBody UserRequest request) {
         return ResponseEntity.ok(userService.saveUserWithBooks(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,30 +1,40 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.UserDTO;
 import com.example.demo.dto.UserRequest;
 import com.example.demo.entity.Book;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, ModelMapper modelMapper) {
         this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
     }
 
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    public List<UserDTO> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(user -> modelMapper.map(user, UserDTO.class))   //entity -> dto
+                .collect(Collectors.toList());
     }
 
-    public User saveUser(User user) {
-        return userRepository.save(user);
+    public UserDTO saveUser(User user) {
+        User savedUser = userRepository.save(user);
+        return modelMapper.map(savedUser, UserDTO.class);   //entity -> dto
+
     }
 
-    public User saveUserWithBooks(UserRequest request) {
+    public UserDTO saveUserWithBooks(UserRequest request) {
         User user = new User();
         user.setName(request.getName());
         user.setEmail(request.getEmail());
@@ -39,7 +49,8 @@ public class UserService {
 
         user.setBooks(books);
 
-        return userRepository.save(user);
+        User savedUser = userRepository.save(user);
+        return modelMapper.map(savedUser, UserDTO.class);
     }
 
 }
